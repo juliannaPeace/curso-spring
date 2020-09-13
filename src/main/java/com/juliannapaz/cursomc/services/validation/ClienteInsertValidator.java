b94ee565f4a854e0cd7de217fb2f25f13e6709de
juliannacev <julianna.plo@grupocev.com>
2020-09-13 15:16:34 -0300

    validação de email

---
+++ src/main/java/com/juliannapaz/cursomc/services/validation/ClienteInsertValidator.java
@@ -0,0 +1,58 @@
+package com.juliannapaz.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.juliannapaz.cursomc.domain.Cliente;
+import com.juliannapaz.cursomc.domain.enums.TipoCliente;
+import com.juliannapaz.cursomc.dto.ClienteNewDTO;
+import com.juliannapaz.cursomc.repositories.ClienteRepository;
+import com.juliannapaz.cursomc.resources.exception.FieldMessage;
+import com.juliannapaz.cursomc.services.validation.utils.BR;
+
+public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Override
+	public void initialize(ClienteInsert ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
+		List<FieldMessage> list = new ArrayList<>();
+
+		if (objDto.getTipoCliente() == null) {
+			list.add(new FieldMessage("Tipo", "tipo não pode ser nulo"));
+		}
+
+		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAFISICA.getCod())
+				&& !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CPF Inválido"));
+		}
+
+		if (objDto.getTipoCliente().equals(TipoCliente.PESSOAJURIDICA.getCod())
+				&& !BR.isValidCPF(objDto.getCpfOuCnpj())) {
+			list.add(new FieldMessage("cpfOuCnpj", "CNPJ Inválido"));
+		}
+
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		if (cliente != null) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+}
---
+++ src/main/java/com/juliannapaz/cursomc/services/validation/ClienteUpdateValidator.java
@@ -0,0 +1,53 @@
+package com.juliannapaz.cursomc.services.validation;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.servlet.HandlerMapping;
+
+import com.juliannapaz.cursomc.domain.Cliente;
+import com.juliannapaz.cursomc.dto.ClienteDTO;
+import com.juliannapaz.cursomc.repositories.ClienteRepository;
+import com.juliannapaz.cursomc.resources.exception.FieldMessage;
+
+public class ClienteUpdateValidator implements ConstraintValidator<ClienteUpdate, ClienteDTO> {
+
+	@Autowired
+	private HttpServletRequest request;
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
+	@Override
+	public void initialize(ClienteUpdate ann) {
+	}
+
+	@Override
+	public boolean isValid(ClienteDTO objDto, ConstraintValidatorContext context) {
+
+		Map<String, String> map = (Map<String, String>) request
+				.getAttribute(HandlerMapping.URI_TEMPLATE_VARIABLES_ATTRIBUTE);
+		Integer uriId = Integer.parseInt(map.get("id"));
+
+		List<FieldMessage> list = new ArrayList<>();
+
+		Cliente cliente = clienteRepository.findByEmail(objDto.getEmail());
+		if (cliente != null && !cliente.getId().equals(uriId)) {
+			list.add(new FieldMessage("email", "Email já existente"));
+		}
+
+		for (FieldMessage e : list) {
+			context.disableDefaultConstraintViolation();
+			context.buildConstraintViolationWithTemplate(e.getMessage()).addPropertyNode(e.getFieldName())
+					.addConstraintViolation();
+		}
+		return list.isEmpty();
+	}
+
+}
